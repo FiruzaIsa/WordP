347a7bba260def26a1de3e1a0b12bd268bf3b7fe
FiruzaIsa <49427451+FiruzaIsa@users.noreply.github.com>
2020-03-27 13:27:38 +0400

    Update archive.php

--- themes/mytheme/archive.php
+++ themes/mytheme/archive.php
@@ -3,7 +3,7 @@
 
 <section class="page-wrap">
 
-<h1><?php echo post_type_archive_title( '', false ); ?></h1>
+<h1 class="text-center"><?php echo post_type_archive_title( '', false ); ?></h1>
 <?php get_template_part('includes/section-service','content');?>
  
 
